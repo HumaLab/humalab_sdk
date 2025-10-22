d332ef58d284ec7310fa880e07c38ea9cece9ea8
Chris La <chris.la@humalab.ai>
2025-10-22 01:08:37 -0700

    [Bug Fix] Fix resource import (#8)
    Fix resource import

--- humalab/assets/__init__.py
+++ humalab/assets/__init__.py
@@ -1,4 +1,4 @@
-from .resource import ResourceManager
+from .resource_manager import ResourceManager
 from .files import ResourceFile, URDFFile
 
 __all__ = ["ResourceManager", "ResourceFile", "URDFFile"]
