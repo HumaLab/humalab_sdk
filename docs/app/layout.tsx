7b08a93e9d27806f619cf5d77834b82ec8e6050e
Chris La <chris.la@humalab.ai>
2025-10-30 22:08:12 -0700

    Refactor docs to root

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,12 +1,23 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
+import { DocsLayout } from 'fumadocs-ui/layout';
 import type { ReactNode } from 'react';
+import { source } from '@/lib/source';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          <DocsLayout
+            tree={source.pageTree}
+            nav={{
+              title: 'HumaLab SDK',
+            }}
+          >
+            {children}
+          </DocsLayout>
+        </RootProvider>
       </body>
     </html>
   );
